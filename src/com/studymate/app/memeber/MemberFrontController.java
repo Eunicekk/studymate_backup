7b7222adbb9f9f58a10962b788dc0a72f7b4a652
higggu <jhguy11@naver.com>
2023-03-28 14:23:48 +0900

    test

--- src/com/studymate/app/memeber/MemberFrontController.java
+++ src/com/studymate/app/memeber/MemberFrontController.java
@@ -10,18 +10,38 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberFrontController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
 	}
 
 	protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String target = req.getRequestURI().substring(req.getContextPath().length());
 		System.out.println(target);
 		
-		switch(target) {
+
+		switch (target) {
+		case "/member/join.me":
+			req.getRequestDispatcher("/app/member/join.jsp").forward(req, resp);
+			break;
+		case "/member/joinOk.me":
+			new JoinOkController().execute(req, resp);
+			break;
+		case "/member/login.me":
+//			new LoginController().execute(req, resp);
+			break;
+		case "/member/loginOk.me":
+//			new LoginOkController().execute(req, resp);
+			break;
+		case "/member/checkIdOk.me":
+//			new CheckIdOkController().execute(req, resp);
+			break;
+		case "/member/logoutOk.me":
+//			new LogoutController().execute(req,resp);
+			break;
 		}
-		
 		}
 }
