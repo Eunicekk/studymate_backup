2690fc33d546491386be6529085bfda5358ed878
leejh1118 <dhtkan12@gmail.com>
2023-04-11 13:40:26 +0900

    no message

--- WebContent/assets/js/portfolioList.js
+++ WebContent/assets/js/portfolioList.js
@@ -5,3 +5,4 @@ const portfolioInfoContainer = document.querySelector(
 portfolioInfoContainer.addEventListener("click", () => {
   window.location.href = "/mypage/MyPagePortfolioOk.my"; // 수정 사이트 URL에는 실제 URL을 입력하세요.
 });
+
