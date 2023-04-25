32dec8c338f63984464fdb9b793e3be79b9ec2a2
leejh1118 <dhtkan12@gmail.com>
2023-04-25 22:41:47 +0900

    no message

--- src/com/studymate/app/memberprofile/dao/MemberProfileDAO.java
+++ src/com/studymate/app/memberprofile/dao/MemberProfileDAO.java
@@ -3,11 +3,16 @@ package com.studymate.app.memberprofile.dao;
 import org.apache.ibatis.session.SqlSession;
 
 import com.mybatis.config.MyBatisConfig;
+import com.studymate.app.memberprofile.dto.MemberProfileDTO;
 
 public class MemberProfileDAO {
 	public SqlSession sqlSession;
 	
 	public MemberProfileDAO(){
 		sqlSession= MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
+	public void insert(MemberProfileDTO memberProfileDTO) {
+		
 	}
 }
